ac784454f0bdf10313771b723b9162c97223014f
kino <359084415@qq.com>
2017-09-09 10:39:06 +0800

    Add Class(ParamChecker) to Project(Command)

---
+++ Sudoku/Sudoku/Command/ParamChecker.h
@@ -0,0 +1,31 @@
+//===============================================================
+//Summary:
+//          ParamChecker ¿‡£¨ 
+//FileName:
+//          ParamChecker.h
+//Remarks:
+//          ...
+//Date:
+//          2017/9/9
+//Author:
+//          kino(359084415@qq.com)
+//===============================================================
+
+#pragma once
+class ParamChecker
+{
+public:
+    
+    //************************************
+    // Method:    ParamChecker
+    // FullName:  ParamChecker::ParamChecker
+    // Access:    public 
+    // Returns:   
+    // Qualifier:
+    //************************************
+    ParamChecker();
+
+
+    ~ParamChecker();
+};
+
